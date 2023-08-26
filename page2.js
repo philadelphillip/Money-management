c23eab12fa0075ac5201c74b52da5dc1131da59c
philadelphillip <142913753+philadelphillip@users.noreply.github.com>
2023-08-26 23:56:46 +0700

    page2.js

--- page2.js
+++ page2.js
@@ -1,14 +1,17 @@
-let percentageCount = 0;
+let percentageCount = 1;
 
-function addingPercent(){
+function addingPercent() {
     const container = document.getElementById('percentage-container');
+    const inputDiv = document.createElement('div');
+    inputDiv.classList.add('percentage-input');
     const input = document.createElement('input');
     input.type = 'number';
-    input.placeholder = 'Enter ${percentageCount + 1} percentage: ';
-    container.appendChild(input);
+    input.placeholder = `Enter ${percentageCount + 1} percentage:`;
+    inputDiv.appendChild(input);
+    container.appendChild(inputDiv);
     percentageCount++;
 }
 
 function navigateToIndex() {
-    win.loadFile('index.html');
+    window.location.href = 'index.html';
 }
